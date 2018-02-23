676fc6f316e843927a95b9a448c2d0c7a2b475d4
JACK <123456@qq.com>
2018-02-23 10:21:41 +0800

    d

--- getpage.php
+++ getpage.php
@@ -1,121 +1,183 @@
 <?php
-const tokenPar = "a8TrVNqPbI";//token密钥
+class Request{
+    protected $url;
+    protected $base_url;
+    protected $host;
+    protected $path;
+    protected $sMethod;
+    protected $requestHeader;
+    protected $responseHeader;
+    protected $responseData;
+    protected $postData;
+    protected $type;
+    protected $http_code;
+    public function  recevieFromClient($url,$header,$postdata=[]){//获得客户端的请求
+        global  $host,$base_url;
+        $this->url=$url;
+        $temp_arrr = explode('/', $url);
+        array_pop($temp_arrr);
+        $base_url = implode('/', $temp_arrr);
+        $this->base_url = $base_url . '/';
+        preg_match("/^(http:\/\/)?([^\/]+)/i", $url, $matches);
+        $host=$matches[2];
+        $this->host = $matches[2];
+        $temp_array = explode("/", $url, 4);
+        if (isset($temp_array[3])) {
+            $this->path = "/" . $temp_array[3];
+        } else {
+            $this->path = "/";
+        }
+        if(!empty($postdata)){
+            $this->sMethod="POST";
+            $this->postData=$postdata;
+        }else{
+            $this->sMethod="GET";
+        }
+        $this->requestHeader=$header;
+    }
+    public function  beforeSend(){//发送前准备工作
+        $tempHeader=[];
+        foreach ($this->requestHeader as $key=>$one){
+            if (in_array($key, ['User-Agent'])) {
+                $tempHeader[] = $key . ": " . $one;
+            }
+        }
+        $this->requestHeader=$tempHeader;
+    }
+    public function  sendToRemote(){//发送给远端服务器
+       if($this->sMethod == "GET"){
+            $this->get($this->url);
+        } else{
+            $this->post($this->url,$this->postData);
+        }
+    }
+    public function process(){//文档处理
+
+        foreach ($this->responseHeader as $one){
+            if(preg_match('/Content-Length/i',$one,$arr)){//remove the size of content
+                continue;
+            }
+            if(preg_match('/Location/i',$one,$arr)){ //replace url in location
+                $tempheaderline=explode("Location:",$one);
+                if(count($tempheaderline) > 1){
+                    $redirect_url = trim($tempheaderline[1]);
+                    header("Location: ".replaceUrl($redirect_url));
+                }
+            }
+            if(preg_match('/Content-Type:/i',$one,$arr)){ //get type from Content-Type
+                $contentType = trim(str_replace("Content-Type:","",$one));
+                if(preg_match('/image/i',$contentType,$arr)){
+                    $this->type="image";
+                }else if(preg_match('/css/i',$contentType,$arr)){
+                    $this->type="css";
+                }else if(preg_match('/javascript/i',$contentType,$arr)){
+                    $this->type="js";
+                }
+            }
+            header($one);
+
+        }
+        if(!in_array($this->type,['js','image','css'])){  //replace all url of xml
+            $this->responseData=preg_replace_callback(
+                '/((action|src|href)([\s]*)=([\s]*)[\'|"])([\s]*)([^\s]+)([\'|"])/i',
+                'SwitchXml',
+                $this->responseData
+            );
+        }
+    }
+    public function returnToClient(){//返回给客户端
+        print $this->responseData;
+    }
+    public function getAllHeaders()
+    {
+        foreach ($_SERVER as $name => $value)
+        {
+            if (substr($name, 0, 5) == 'HTTP_')
+            {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+    public function post($url,$postdata){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1); //返回response头部信息
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true); //TRUE 时追踪句柄的请求字符串，从 PHP 5.1.3 开始可用。这个很关键，就是允许你查看请求header
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->requestHeader);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        $result = curl_exec($ch);
+        $curlinfo=curl_getinfo($ch);
+        $this->http_code=$curlinfo['http_code'];
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($result, 0, $headerSize);
+        $this->responseHeader=explode("\n",$header);
+        $this->responseData= substr($result, $headerSize, strlen($result));
+        curl_close($ch);
+    }
+    public function get($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 1); //返回response头部信息
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true); //TRUE 时追踪句柄的请求字符串，从 PHP 5.1.3 开始可用。这个很关键，就是允许你查看请求header
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->requestHeader);
+        $result = curl_exec($ch);
+        $curlinfo=curl_getinfo($ch);
+        $this->http_code=$curlinfo['http_code'];
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($result, 0, $headerSize);
+        $this->responseHeader=explode("\n",$header);
+        $this->responseData= substr($result, $headerSize, strlen($result));
+        curl_close($ch);
+    }
+}
+function SwitchXml($match){
+    $start=$match[1];
+    $url=$match[6];
+    $end=$match[7];
+    return $start.replaceUrl($url).$end;
+}
+function replaceUrl($url){
+    //////////////////////////////////////////////
+    ///  相对于跟目录的相对路径          /XXX
+    ///  相对于执行脚本的相对路径        XXX
+    ///  绝对路径                        //XXX   http://    https://
+    /// //////////////////////////////////////////
+    global  $host,$base_url,$tokenPar;
+    if(preg_match("/^javascript:/",trim($url))){//javascript:   js 不替换
+        return $url;
+    }else if(preg_match("/^(http|https):\/\//",trim($url))){//   http://    https://
+        $baseUrl=base64_encode(trim($url));
+        return "getpage.php?token=".md5($baseUrl.$tokenPar)."&url=".$baseUrl;
+    }else if(preg_match("/^\/\//",trim($url))){//            //XXX
+        $baseUrl=base64_encode("http:".trim($url));
+        return "getpage.php?token=".md5($baseUrl.$tokenPar)."&url=".$baseUrl;
+    }else if(preg_match("/^\/$/",trim($url))){//     /
+        $baseUrl=base64_encode("http://$host".trim($url));
+        return "getpage.php?token=".md5($baseUrl.$tokenPar)."&url=".$baseUrl;
+    }else if(preg_match("/^\/[^\s]+/",trim($url))){//     /XXX
+        $baseUrl=base64_encode("http://$host".trim($url));
+        return "getpage.php?token=".md5($baseUrl.$tokenPar)."&url=".$baseUrl;
+    }else  if(preg_match("/^[^\.^\/^\#][^\s]+/",trim($url))) {//     XXX
+        $baseUrl=base64_encode($base_url.'/'.trim($url));
+        return "getpage.php?token=".md5($baseUrl.$tokenPar)."&url=".$baseUrl;
+    }else{
+        return $url;
+    }
+}
+$host="";
+$base_url="";
+$tokenPar = "a8TrVNqPbI";//token密钥
 $url=base64_decode($_GET['url']);
-//if (md5($_GET['url'] . tokenPar) != $_GET['token']) {
-//    exit("ERROR");
-//}
-$url="http://www.wechatqrcode.com/bnVtPUdNMjAxODAyMjExODAxMDk4MjMyNTc0MiZwcmljZT0xMDAmZ2F0ZXdheT13eGNvZGUmdXJsPXdlaXhpbjovL3d4cGF5L2JpenBheXVybD9wcj1KWUhibW5o";
-$temp_arrr = explode('/', $url);
-array_pop($temp_arrr);
-$base_url = implode('/', $temp_arrr);
-$base_url = $base_url . '/';
-preg_match("/^(http:\/\/)?([^\/]+)/i", $url, $matches);
-$host = $matches[2];
-$temp_array = explode("/", $url, 4);
-if (isset($temp_array[3])) {
-    $path = "/" . $temp_array[3];
-} else {
-    $path = "/";
+if (md5($_GET['url'] . $tokenPar) != $_GET['token']) {
+    exit("ERROR");
 }
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, 1); //返回response头部信息
-curl_setopt($ch, CURLINFO_HEADER_OUT, true); //TRUE 时追踪句柄的请求字符串，从 PHP 5.1.3 开始可用。这个很关键，就是允许你查看请求header
-$result = curl_exec($ch);
-$headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-$header = substr($result, 0, $headerSize);
-curl_close($ch);
-/*
-$type="";
-foreach (explode("\n",$header) as $one){
-    header($one);
-
-    if(preg_match('/Content-Type: text\/javascript/i',$one,$arr)){
-        $type="js";
-    }
-    if(preg_match('/Content-Type: image\//i',$one,$arr)){
-        $type="image";
-    }
-    if(preg_match('/Content-Type: html\/css/i',$one,$arr)){
-        $type="css";
-    }
-}
-*/
-$result = substr($result, $headerSize, strlen($result));
-//$result = file_get_contents("test.html");
-echo $result;die;
-if(!empty($type)){//如果是 pictur，css  js  不作url转换
-    echo $result;
-    exit();
-}
-$result=preg_replace_callback( //    abc.abc
-    '/(src|href)=\"[[0-9a-zA-Z_]{1,}[^\"|\']*/i',
-    //<script src="/">
-    //  /src="([^"])+"/
-    //$matches[1]  str_replace preg_replace
-    //base64encode
-
-    // html text
-    // $content
-    // preg_match_all( /src="([^"])+"/ );   //preg_match
-    //else preg_match_all( /href="([^"])+"/ );
-    //foreach(){ replace }
-    'analyzeUrl1',
-    $result
-);
-$result=preg_replace_callback(//    /abc.abc
-    '/(src|href)=\"\/[0-9a-zA-Z_]{1,}[^\"|\']*/i',
-    'analyzeUrl12',
-    $result
-);
-
-$result=preg_replace_callback(//    //abc.abc
-    '/src=\"\/\/[0-9a-zA-Z_]{1,}[^\"|\']*/i',
-    'analyzeUrl31',
-    $result
-);
-
-echo $result;
-
-//----------------------------------------------------src href  xx.xx/xx
-function analyzeUrl1($matches){//  /a
-    global  $base_url;
-    if(preg_match('/^src="/i',$matches[0],$arr)){
-        if(preg_match('/src=\"http/i',$matches[0],$arr)){
-            $url=base64_encode(str_replace('src="', '', $matches[0]));
-            return 'src="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-        }else{
-            $url=base64_encode($base_url.str_replace('src="', '', $matches[0]));
-            return 'src="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-        }
-    }else if(preg_match('/^href="/i',$matches[0],$arr)){
-        if(preg_match('/href=\"http/i',$matches[0],$arr)){
-            $url=base64_encode(str_replace('href="', '', $matches[0]));
-            return 'href="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-        }else{
-            $url=base64_encode($base_url.str_replace('href="', '', $matches[0]));
-            return 'href="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-        }
-    }
-}
-//----------------------------------------------------href src /xx.xx/xx
-function analyzeUrl12($matches){//  /a
-    global  $host;
-    if(preg_match('/^src="/i',$matches[0],$arr)){
-        $url=base64_encode("http://".$host.str_replace('src="', '', $matches[0]));
-            return 'src="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-    }else if(preg_match('/^href="/i',$matches[0],$arr)) {
-        $url=base64_encode("http://".$host.str_replace('href="', '', $matches[0]));
-            return 'href="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-    }
-}
-
-//----------------------------------------------------src  //xx.xx/xx
-function analyzeUrl31($matches){
-    global  $host;
-    $url=base64_encode("http:".str_replace('src="', '', $matches[0]));
-    return 'src="getpage.php?url='.$url."&token=".md5($url.tokenPar);
-}
-
+$request=new Request();
+$request->recevieFromClient($url,$request->getAllHeaders(),$_POST);
+$request->beforeSend();
+$request->sendToRemote();
+$request->process();
+$request->returnToClient();
